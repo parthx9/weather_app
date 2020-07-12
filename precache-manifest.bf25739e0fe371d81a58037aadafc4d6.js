83ff0f7b027fe6c2dfdf159080c0c10e4271eeff
ParthArora <partharora.india@gmail.com>
2020-07-13 01:16:01 +0530

    Updates

---
+++ precache-manifest.bf25739e0fe371d81a58037aadafc4d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783b692ba8bae3f57809d30934c43755",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "29799b8b9b4f99a1f218",
+    "url": "/weather-app/static/css/main.815fcbe0.chunk.css"
+  },
+  {
+    "revision": "c15d4796229d148464a4",
+    "url": "/weather-app/static/js/2.b88b163c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.b88b163c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29799b8b9b4f99a1f218",
+    "url": "/weather-app/static/js/main.883e4d4e.chunk.js"
+  },
+  {
+    "revision": "143bf1c3ab01288e5a6c",
+    "url": "/weather-app/static/js/runtime-main.13c47b1c.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
